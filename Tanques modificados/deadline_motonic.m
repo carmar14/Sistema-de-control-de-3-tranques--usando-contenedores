04dbcc1ecbd8e5345f6fbb6d6e29fa9dde6670c3
carmar14 <carmar141414@hotmail.com>
2021-12-15 10:52:45 -0500

    Algoritmos de planificabilidad

---
+++ Tanques modificados/deadline_motonic.m
@@ -0,0 +1,59 @@
+clc
+close all
+clear
+
+T=[3 3 10000 1000 1000]';
+C=[1 1 17 37 158]';
+D=[3 3 500 1000 1000]';
+
+%ceil para redondear hacia arriba
+
+R=zeros(5,1);
+
+Ra=C;
+Rn=R;
+I=0;
+for i=1:5
+    while (Rn(i)~=Ra(i))
+        
+        Ra(i) = Rn(i);
+       
+        for j=1:i-1
+            I=I+ceil(Ra(i)/T(j))*C(j);
+        end
+
+        Rn(i) = C(i)+I;
+        I=0;
+
+    end
+end
+
+
+%-------------tareas del proyecto--------
+
+
+T=[1 1 1]';
+C=[0.6 0.375 0.6]';
+D=[0.75 0.47 0.75]';
+
+%ceil para redondear hacia arriba
+
+R=zeros(3,1);
+
+Ra=C;
+Rn=R;
+I=0;
+for i=1:3
+    while (Rn(i)~=Ra(i))
+        
+        Ra(i) = Rn(i);
+       
+        for j=1:i-1
+            I=I+ceil(Ra(i)/T(j))*C(j);
+        end
+
+        Rn(i) = C(i)+I;
+        I=0;
+
+    end
+end
