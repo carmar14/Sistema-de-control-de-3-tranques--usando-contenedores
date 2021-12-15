cb212741eb734904c480989744c3acf2e9bfdc42
carmar14 <carmar141414@hotmail.com>
2021-12-15 15:37:20 -0500

    Deadline min
    Resultados caso de estudio de tesis de Diego

--- Tanques modificados/deadline_ej.m
+++ Tanques modificados/deadline_ej.m
@@ -41,9 +41,122 @@ end
 
 
 Dmin=[0 0 0];
-%---------nueva minimizacion para T2----
 deadline=C;
+%---------nueva minimizacion para T2----
+
 i=2;
+% for i=1:3
+for s=0:k-1
+    t=min(s*P(i)+D(i),R);
+    
+    deadline(i)=C(i);
+    while t>(s*P(i)+C(i))
+        H=0;
+        for j=1:3
+            H=H+C(j)*floor((t+P(j)-D(j))/P(j)); % calcula de H(t)
+        end
+        
+        if (t-H)<C(i)
+            deadline(i)=H+C(i)-s*P(i);
+            break;
+        end
+        t=t-1;
+        
+    end
+    Dmin(i)=max(Dmin(i),deadline(i));
+    
+end
+
+D(i)=Dmin(i);
+
+%---------nueva minimizacion para T1----
+i=1;
+% for i=1:3
+for s=0:k-1
+    t=min(s*P(i)+D(i),R);
+    
+    deadline(i)=C(i);
+    while t>(s*P(i)+C(i))
+        H=0;
+        for j=1:3
+            H=H+C(j)*floor((t+P(j)-D(j))/P(j));
+        end
+        
+        if (t-H)<C(i)
+            deadline(i)=H+C(i)-s*P(i);
+            break;
+        end
+        t=t-1;
+        
+    end
+    Dmin(i)=max(Dmin(i),deadline(i));
+    
+end
+
+D(i)=Dmin(i);
+
+%---------nueva minimizacion para T3----
+i=3;
+% for i=1:3
+for s=0:k-1
+    t=min(s*P(i)+D(i),R);    
+    deadline(i)=C(i);
+    while t>(s*P(i)+C(i))
+        H=0;
+        for j=1:3
+            H=H+C(j)*floor((t+P(j)-D(j))/P(j));
+        end
+        
+        if (t-H)<C(i)
+            deadline(i)=H+C(i)-s*P(i);
+            break;
+        end
+        t=t-1;
+        
+    end
+    Dmin(i)=max(Dmin(i),deadline(i));
+    
+end
+
+D(i)=Dmin(i);
+%------------ejemplo sistema de pendulos-----
+
+C=[7 7 7];
+D=[20 29 35];
+P=[20 29 35];
+
+K=1;
+
+G=0;
+for i=1:3
+    G=G++ceil(K/P(i))*C(i);
+end
+K_=G;
+G=0;
+
+
+%--------calcular ICI-->R---
+
+while (K_~=K)
+    K=K_;
+    for i=1:3
+        G=G++ceil(K/P(i))*C(i);
+    end
+    K_=G;
+    G=0;
+end
+
+R=K_;
+for i=1:3
+    k(i)=ceil(R/P(i));
+end
+
+
+Dmin=[0 0 0];
+deadline=C;
+%---------nueva minimizacion para T3----
+
+i=3;
 % for i=1:3
 for s=0:k-1
     t=min(s*P(i)+D(i),R);
@@ -90,8 +203,8 @@ end
 
 D(i)=Dmin(i);
 
-%---------nueva minimizacion para T3----
-i=3;
+%---------nueva minimizacion para T2----
+i=2;
 % for i=1:3
 for s=0:k-1
     t=min(s*P(i)+D(i),R);
--- Tanques modificados/deadline_min.m
+++ Tanques modificados/deadline_min.m
@@ -2,95 +2,348 @@ clc
 close all
 clear
 
-Tm=110;
-Dm=110;
-Dc=110;
-Da=110;
-D= [Dm Dc Da];
-deadline = [1.5 5 0.3];
-WCET = [1.5 5 0.3];
-Dmin = [0 0 0];
+%-------Nodo medición-----
+C=[15 30 30];
+D=[1100 100 100];
+P=[1100 100 100];
 
-%----tareas adicionales----
-WCETi=3;
-Di=10;
-Ti=10;
+K=1;
 
-
-%-------------tareas del proyecto--------
-
-
-T=Tm*[1 1 1]';
-C=[1.5 5 0.3]';
-%ceil para redondear hacia arriba
-
-R=zeros(3,1);
-
-Ra=C;
-Rn=R;
-I=0;
-for i=1:3
-    while (Rn(i)~=Ra(i))
-        
-        Ra(i) = Rn(i);
-       
-        for j=1:i-1
-            I=I+ceil(Ra(i)/T(j))*C(j);
-        end
-
-        Rn(i) = C(i)+I;
-        I=0;
-
-    end
-end
-
-R=Rn;
-k=ceil(R./T);
-j=length(0:0.1:R(1));
-
-%------test de factibilidad Ripoll-- 
-t=0;
-C=[1.5 3 3]; %medicion
-D=0.8*[110 10 10];
-P=[110 10 10];
-c=sum(C./P);
-gama= sum(C.*(1-D./P));
-L= gama/(1-c);
 G=0;
-H=0;
-for i =1:3
-    G=G+C(i)*ceil(t/P(i));
-    H=H+C(i)*floor((t+P(i)-D(i))/P(i));
-end
-
-while (t<=min(G,L) && t>=H)         
-    t=t+1;
-    for i =1:3
-    G=G+C(i)*ceil(t/P(i));
-    H=H+C(i)*floor((t+P(i)-D(i))/P(i));
-    end  
-end
-
-for t=0:0.1:R(1)
-    for i=1:3
-        H(j)=WCET(i)*floor((t/110));
-        j=j+1;
-    end
-end
-
-
-
 for i=1:3
-    for s=0:k-1
-        t=min(s*T(i)+D(i),R(i));
-        deadline = C;
-        while(t>s*T(i)+C(i))%deadline(i))
-            H(t)=4;
-            if(t-H(t)<C(i))
-                deadline(i)=H(i)+WCET(i)-s*T(i);
-            end
-        t=t-1;
-        end
-        Dmin=max(Dmin(i),deadline(i));
-    end
+    G=G++ceil(K/P(i))*C(i);
 end
+K_=G;
+G=0;
+
+
+%--------calcular ICI-->R---
+
+while (K_~=K)
+    K=K_;
+    for i=1:3
+        G=G++ceil(K/P(i))*C(i);
+    end
+    K_=G;
+    G=0;
+end
+
+R=K_;
+for i=1:3
+    k(i)=ceil(R/P(i));
+end
+
+
+Dmin=[0 0 0];
+deadline=C;
+%---------nueva minimizacion para T2----
+
+i=1;
+% for i=1:3
+for s=0:k-1
+    t=min(s*P(i)+D(i),R);
+    
+    deadline(i)=C(i);
+    while t>(s*P(i)+C(i))
+        H=0;
+        for j=1:3
+            H=H+C(j)*floor((t+P(j)-D(j))/P(j)); % calcula de H(t)
+        end
+        
+        if (t-H)<C(i)
+            deadline(i)=H+C(i)-s*P(i);
+            break;
+        end
+        t=t-1;
+        
+    end
+    Dmin(i)=max(Dmin(i),deadline(i));
+    
+end
+
+D(i)=Dmin(i);
+
+%---------nueva minimizacion para T2----
+i=2;
+% for i=1:3
+for s=0:k-1
+    t=min(s*P(i)+D(i),R);
+    
+    deadline(i)=C(i);
+    while t>(s*P(i)+C(i))
+        H=0;
+        for j=1:3
+            H=H+C(j)*floor((t+P(j)-D(j))/P(j));
+        end
+        
+        if (t-H)<C(i)
+            deadline(i)=H+C(i)-s*P(i);
+            break;
+        end
+        t=t-1;
+        
+    end
+    Dmin(i)=max(Dmin(i),deadline(i));
+    
+end
+
+D(i)=Dmin(i);
+
+%---------nueva minimizacion para T3----
+i=3;
+% for i=1:3
+for s=0:k-1
+    t=min(s*P(i)+D(i),R);    
+    deadline(i)=C(i);
+    while t>(s*P(i)+C(i))
+        H=0;
+        for j=1:3
+            H=H+C(j)*floor((t+P(j)-D(j))/P(j));
+        end
+        
+        if (t-H)<C(i)
+            deadline(i)=H+C(i)-s*P(i);
+            break;
+        end
+        t=t-1;
+        
+    end
+    Dmin(i)=max(Dmin(i),deadline(i));
+    
+end
+
+D(i)=Dmin(i);
+D_=zeros(3,3);
+D_(1,:)=D/10;
+
+%----------Nodo medición---------
+
+%----------Nodo control---------
+C=[5 3 3];
+D=[110 10 10];
+P=[110 10 10];
+
+K=1;
+
+G=0;
+for i=1:3
+    G=G++ceil(K/P(i))*C(i);
+end
+K_=G;
+G=0;
+
+
+%--------calcular ICI-->R---
+
+while (K_~=K)
+    K=K_;
+    for i=1:3
+        G=G++ceil(K/P(i))*C(i);
+    end
+    K_=G;
+    G=0;
+end
+
+R=K_;
+for i=1:3
+    k(i)=ceil(R/P(i));
+end
+
+
+Dmin=[0 0 0];
+deadline=C;
+%---------nueva minimizacion para T2----
+
+i=1;
+% for i=1:3
+for s=0:k-1
+    t=min(s*P(i)+D(i),R);
+    
+    deadline(i)=C(i);
+    while t>(s*P(i)+C(i))
+        H=0;
+        for j=1:3
+            H=H+C(j)*floor((t+P(j)-D(j))/P(j)); % calcula de H(t)
+        end
+        
+        if (t-H)<C(i)
+            deadline(i)=H+C(i)-s*P(i);
+            break;
+        end
+        t=t-1;
+        
+    end
+    Dmin(i)=max(Dmin(i),deadline(i));
+    
+end
+
+D(i)=Dmin(i);
+
+%---------nueva minimizacion para T2----
+i=2;
+% for i=1:3
+for s=0:k-1
+    t=min(s*P(i)+D(i),R);
+    
+    deadline(i)=C(i);
+    while t>(s*P(i)+C(i))
+        H=0;
+        for j=1:3
+            H=H+C(j)*floor((t+P(j)-D(j))/P(j));
+        end
+        
+        if (t-H)<C(i)
+            deadline(i)=H+C(i)-s*P(i);
+            break;
+        end
+        t=t-1;
+        
+    end
+    Dmin(i)=max(Dmin(i),deadline(i));
+    
+end
+
+D(i)=Dmin(i);
+
+%---------nueva minimizacion para T3----
+i=3;
+% for i=1:3
+for s=0:k-1
+    t=min(s*P(i)+D(i),R);    
+    deadline(i)=C(i);
+    while t>(s*P(i)+C(i))
+        H=0;
+        for j=1:3
+            H=H+C(j)*floor((t+P(j)-D(j))/P(j));
+        end
+        
+        if (t-H)<C(i)
+            deadline(i)=H+C(i)-s*P(i);
+            break;
+        end
+        t=t-1;
+        
+    end
+    Dmin(i)=max(Dmin(i),deadline(i));
+    
+end
+
+D(i)=Dmin(i);
+D_(2,:)=D;
+
+%----------Nodo control---------
+
+%----------Nodo actuación---------
+C=10*[0.3 3 3];
+D=10*[110 10 10];
+P=10*[110 10 10];
+
+K=1;
+
+G=0;
+for i=1:3
+    G=G++ceil(K/P(i))*C(i);
+end
+K_=G;
+G=0;
+
+
+%--------calcular ICI-->R---
+
+while (K_~=K)
+    K=K_;
+    for i=1:3
+        G=G++ceil(K/P(i))*C(i);
+    end
+    K_=G;
+    G=0;
+end
+
+R=K_;
+for i=1:3
+    k(i)=ceil(R/P(i));
+end
+
+
+Dmin=[0 0 0];
+deadline=C;
+%---------nueva minimizacion para T2----
+
+i=1;
+% for i=1:3
+for s=0:k-1
+    t=min(s*P(i)+D(i),R);
+    
+    deadline(i)=C(i);
+    while t>(s*P(i)+C(i))
+        H=0;
+        for j=1:3
+            H=H+C(j)*floor((t+P(j)-D(j))/P(j)); % calcula de H(t)
+        end
+        
+        if (t-H)<C(i)
+            deadline(i)=H+C(i)-s*P(i);
+            break;
+        end
+        t=t-1;
+        
+    end
+    Dmin(i)=max(Dmin(i),deadline(i));
+    
+end
+
+D(i)=Dmin(i);
+
+%---------nueva minimizacion para T2----
+i=2;
+% for i=1:3
+for s=0:k-1
+    t=min(s*P(i)+D(i),R);
+    
+    deadline(i)=C(i);
+    while t>(s*P(i)+C(i))
+        H=0;
+        for j=1:3
+            H=H+C(j)*floor((t+P(j)-D(j))/P(j));
+        end
+        
+        if (t-H)<C(i)
+            deadline(i)=H+C(i)-s*P(i);
+            break;
+        end
+        t=t-1;
+        
+    end
+    Dmin(i)=max(Dmin(i),deadline(i));
+    
+end
+
+D(i)=Dmin(i);
+
+%---------nueva minimizacion para T3----
+i=3;
+% for i=1:3
+for s=0:k-1
+    t=min(s*P(i)+D(i),R);    
+    deadline(i)=C(i);
+    while t>(s*P(i)+C(i))
+        H=0;
+        for j=1:3
+            H=H+C(j)*floor((t+P(j)-D(j))/P(j));
+        end
+        
+        if (t-H)<C(i)
+            deadline(i)=H+C(i)-s*P(i);
+            break;
+        end
+        t=t-1;
+        
+    end
+    Dmin(i)=max(Dmin(i),deadline(i));
+    
+end
+
+D(i)=Dmin(i);
+D_(3,:)=D/10;
+
+%----------Nodo actuación---------
